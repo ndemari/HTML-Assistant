e0ee70d02315267523c8d6c468e3a71df56dca18
Nick Demari <demarily@outlook.com>
2015-06-07 16:35:59 -0500

    Initial submit to git

---
+++ HTML Assistant/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim FilePath As String = InputBox("Folder for import")
+        Dim di As New DirectoryInfo(FilePath)
+        Dim fileJPG As FileInfo()
+        Dim strNEFs As New List(Of String)
+
+        Dim outFile As StreamWriter
+        'Dim inFile As StreamReader
+
+        outFile = File.AppendText("HTML.txt")
+        fileJPG = di.GetFiles("*.JPG")
+
+        For Each JPG In fileJPG
+            Dim intSub As Integer = JPG.Name.IndexOf(".")
+            Dim strParsedJPG As String = JPG.Name.Substring(0, intSub)
+            outFile.WriteLine("<a href=""img/" & JPG.Name & """ data-lightbox=""" & strParsedJPG & "a""></a>")
+        Next
+
+        outFile.Close()
+
+    End Sub
+End Class
---
+++ HTML Assistant/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("HTML Assistant")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("HTML Assistant")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4f9a29ae-5242-46ac-91b2-a16d4dc60477")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
